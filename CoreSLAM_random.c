b12153b2c6ac222f73ab82a717391baad6a75c29
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2016-03-23 08:47:54 +0100

    Fix random generator on ARM architecture

--- CoreSLAM_random.c
+++ CoreSLAM_random.c
@@ -44,7 +44,11 @@ float ts_random_normal(ts_randomizer_t *d, float m, float s)
     d->hz = SHR3(d); 
     d->iz = d->hz & 127;
     x= ((unsigned long)abs(d->hz) < d->kn[d->iz])? d->hz * d->wn[d->iz] : ts_random_normal_fix(d); // Generic version
-    x *= .2328306e-9;  // Normalize the random numbers // printf("d->hz: %d .... x: %d\n", d->hz, x);
+#ifndef __arm__
+//    printf("--d->hz: %d .... x: %f\n", d->hz, x);
+    x *= .2328306e-9;  // Normalize the random numbers
+//    printf("d->hz: %d .... x: %f\n", d->hz, x);
+#endif
     return x * s + m ;
 };
 
